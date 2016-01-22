56bc1ade0cabddfe43e5c9c7652ba13a53a6c391
Kiril Vatev <vatev.1@gmail.com>
2016-01-22 16:52:55 -0500

    adding initial stuff

---
+++ duplicate-reporter.js
@@ -0,0 +1,48 @@
+module.exports = DuplicateReporter;
+
+function DuplicateReporter(runner) {
+    var passes = 0;
+    var failures = 0;
+    
+    var tests = {};
+    var duplicates = {};
+    
+    function onTestComplete(title) {
+        if (tests[title]) {
+            duplicates[title] = true;
+        } else {
+            tests[title] = true;
+        }
+    }
+    
+    function onEnd() {
+        var duplicateList = Object.keys(duplicates);
+        
+        console.log('');
+        console.log('%d total tests: \n   %d passed \n   %d failed \n   %d duplicate names', passes + failures, passes, failures, duplicateList.length);
+        
+        if (duplicateList.length) {
+            console.log('');
+            console.log('Duplicate test names:');
+            console.log(Object.keys(duplicates).join('\n'));
+        }
+        
+        process.exit(failures);
+    }
+    
+    runner.on('pass', function (test) {
+        passes++;
+        onTestComplete(test.fullTitle());
+        // console.log('pass: %s', test.fullTitle());
+    });
+
+    runner.on('fail', function (test, err) {
+        failures++;
+        onTestComplete(test.fullTitle());
+        // console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
+    });
+
+    runner.on('end', function () {
+        setTimeout(onEnd, 0);
+    });
+}
