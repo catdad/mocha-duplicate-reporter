92add12a0e590ccf74c910791be591985ea7053b
Kiril Vatev <vatev.1@gmail.com>
2016-01-22 21:39:53 -0500

    adding pending test reporting

--- duplicate-reporter.js
+++ duplicate-reporter.js
@@ -3,6 +3,7 @@ module.exports = DuplicateReporter;
 function DuplicateReporter(runner) {
     var passes = 0;
     var failures = 0;
+    var pending = 0;
     
     var tests = {};
     var duplicates = {};
@@ -17,12 +18,17 @@ function DuplicateReporter(runner) {
     
     function onEnd() {
         var duplicateList = Object.keys(duplicates);
-        var total = passes + failures;
+        var total = passes + failures + pending;
         
         console.log('');
-        console.log('Regex report: %d-%d-%d-%d', total, passes, failures, duplicateList.length);
+        console.log('Regex report:', [total, passes, failures, pending, duplicateList.length].join('-'));
         console.log('');
-        console.log('%d total tests: \n   %d passed \n   %d failed \n   %d duplicate names', total, passes, failures, duplicateList.length);
+        console.log(
+            '%d total tests:' + 
+            '\n   %d passed' + 
+            '\n   %d failed' + 
+            '\n   %d pending' + 
+            '\n   %d duplicate names', total, passes, failures, pending, duplicateList.length);
         
         if (duplicateList.length) {
             console.log('');
@@ -43,8 +49,13 @@ function DuplicateReporter(runner) {
         failures++;
         onTestComplete(test.fullTitle());
     });
+    
+    runner.on('pending', function(test) {
+        pending++;
+        onTestComplete(test.fullTitle());
+    });
 
     runner.on('end', function () {
-        setTimeout(onEnd, 0);
+        onEnd();
     });
 }
